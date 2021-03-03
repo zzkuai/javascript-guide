6e8c5ec8c372814ff601077ff00e451e408f9cc2
zzkuai <zzkuai@gmail.com>
2021-03-03 22:31:33 +0800

    feat: 添加类型

---
+++ type.js
@@ -0,0 +1,19 @@
+/**
+ * 类型
+ */
+
+// 原始类型
+var str = "1"; // string
+var num = 1; // number
+var bol = true; // boolean
+var nul = null; // null
+var und = undefined; // undefined
+var sym = Symbol(1); // Symbol
+var big = 9007199254740992n; // BigInt
+
+// 复杂类型
+var obj = { a: 1 }; // object
+var arr = [1, 2, 3]; // array
+var func = function () {
+  return 1;
+}; // function
