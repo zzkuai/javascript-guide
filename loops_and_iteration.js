7b570d10e3d3d649346cc181b7cd1ee6fe143ce7
zzkuai <zzkuai@gmail.com>
2021-03-11 22:56:45 +0800

    feat: 添加循环与迭代

---
+++ loops_and_iteration.js
@@ -0,0 +1,37 @@
+/**
+ * 循环与迭代
+ */
+
+/// for 语句
+// 一个 for 循环会一直重复执行，直到指定的循环条件为 false。
+// for ([initialExpression]; [condition]; [incrementExpression])
+//  statement
+
+/// do...while 语句 -- 至少执行一次
+// do
+//  statement
+// while (condition);
+
+/// while 语句
+// while (condition)
+//  statement
+
+/// label 语句
+// label:
+//  statement
+
+/// break 语句
+// break [label];
+
+/// continue 语句
+// continue [label];
+
+/// for...in 语句 -- for...in 语句循环一个指定的变量来循环一个对象所有可枚举的属性。
+// for (variable in object) {
+//  statements
+// }
+
+/// for...of 语句 -- for...of 语句在可迭代对象（包括Array、Map、Set、arguments 等等）上创建了一个循环，对值的每一个独特属性调用一次迭代。
+// for (variable of object) {
+//   statement
+// }
